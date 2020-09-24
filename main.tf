108ed1d10df1525e5e13daca35fdaae5129d41fe
Andreas Wittig <andreas@widdix.de>
2020-09-24 16:00:06 +0200

    workaround for SNS delay

--- main.tf
+++ main.tf
@@ -105,9 +105,6 @@ resource "aws_sns_topic_subscription" "marbot" {
     "numRetries": 100,
     "numNoDelayRetries": 0,
     "backoffFunction": "exponential"
-  },
-  "throttlePolicy": {
-    "maxReceivesPerSecond": 1
   }
 }
 JSON
@@ -135,7 +132,7 @@ resource "aws_cloudwatch_event_target" "monitoring_jump_start_connection" {
 {
   "Type": "monitoring-jump-start-tf-connection",
   "Module": "basic",
-  "Version": "0.9.1",
+  "Version": "0.10.0",
   "Partition": "${data.aws_partition.current.partition}",
   "AccountId": "${data.aws_caller_identity.current.account_id}",
   "Region": "${data.aws_region.current.name}"
