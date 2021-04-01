a2149687c273e6436fe442c1446ac364e7243c4c
Richard Worwood <30800383+RichardWorwood@users.noreply.github.com>
2021-04-01 11:05:56 +0100

    Updated provider version support  (#3)

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">= 0.12.0"
   required_providers {
-    aws    = ">= 2.48.0, < 3"
-    random = "~> 2.2"
+    aws    = ">= 2.48.0"
+    random = ">= 2.2"
   }
 }
 
