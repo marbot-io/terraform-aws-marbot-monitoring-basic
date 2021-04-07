9d91d00318c89b62f4d9cb999427a9c54c02c088
Richard Worwood <30800383+RichardWorwood@users.noreply.github.com>
2021-04-07 17:07:19 +0100

    add kms key variable

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "module_name" {
+  type        = string
+  description = "marbot module name"
+  default     = "marbot-monitoring-basic"
+}
+
 variable "endpoint_id" {
   type        = string
   description = "Your marbot endpoint ID (to get this value: select a channel where marbot belongs to and send a message like this: \"@marbot show me my endpoint id\")."
