2ccf4cdcf45d332fc9a6c2261602490da1c1a3a4
Michael Wittig <michael@widdix.de>
2023-05-08 09:09:59 +0200

    bump version

--- main.tf
+++ main.tf
@@ -142,7 +142,7 @@ resource "aws_cloudwatch_event_target" "monitoring_jump_start_connection" {
 {
   "Type": "monitoring-jump-start-tf-connection",
   "Module": "basic",
-  "Version": "0.26.0",
+  "Version": "0.27.0",
   "Partition": "${data.aws_partition.current.partition}",
   "AccountId": "${data.aws_caller_identity.current.account_id}",
   "Region": "${data.aws_region.current.name}"
